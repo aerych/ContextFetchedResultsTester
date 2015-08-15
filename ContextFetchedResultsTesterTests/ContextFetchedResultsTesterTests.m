9843c49773c016011135e752b7f224b8f33cb3aa
aerych <aerychj@gmail.com>
2015-08-15 17:34:40 -0500

    Initial Commit

---
+++ ContextFetchedResultsTesterTests/ContextFetchedResultsTesterTests.m
@@ -0,0 +1,40 @@
+//
+//  ContextFetchedResultsTesterTests.m
+//  ContextFetchedResultsTesterTests
+//
+//  Created by aerych on 8/15/15.
+//  Copyright (c) 2015 aerych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ContextFetchedResultsTesterTests : XCTestCase
+
+@end
+
+@implementation ContextFetchedResultsTesterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
